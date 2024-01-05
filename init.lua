d248b884ad39d7c555334454cc8a23dd622e030a
reg77777 <29976655+reg77777@users.noreply.github.com>
2024-01-05 17:04:56 +0900

    細かいところを修正

--- init.lua
+++ init.lua
@@ -1,9 +1,3 @@
-vim.opt.helplang='ja','en'
-vim.o.clipboard = vim.o.clipboard .. 'unnamedplus'
-vim.o.syntax = true
-
 require('lazy-nvim')
 require('option')
-require('key')
-
---vim.cmd[[colorscheme molokai]]
+require('keymap')
--- lua/option.lua
+++ lua/option.lua
@@ -1,6 +1,12 @@
+vim.opt.helplang='ja','en'
+
 vim.scriptencoding = 'utf-8'
 vim.opt.encoding = 'utf-8'
 vim.opt.fileencoding = 'utf-8'
+
+-- clipboard
+vim.o.clipboard = vim.o.clipboard .. 'unnamedplus'
+vim.o.syntax = true
 
 vim.opt.backup = false
 vim.opt.swapfile = false
